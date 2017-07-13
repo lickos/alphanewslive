749e252f53ab23e89432200a896387bde0f04a98
lickos <george.chios@gmail.com>
2017-07-13 20:20:18 +0300

    Sixth Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,35 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { KatigoriaViewPage } from '../pages/katigoria-view/katigoria-view';
 import { ArtcleViewPage } from '../pages/artcle-view/artcle-view';
 import { Splash } from '../pages/splash/splash';
+import { FavoritesPage } from '../pages/favorites/favorites';
 
 import { StatusBar } from '@ionic-native/status-bar';
 
 @NgModule({
-  declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage, KatigoriaViewPage, ArtcleViewPage, Splash],
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    FavoritesPage,
+    KatigoriaViewPage,
+    ArtcleViewPage,
+    Splash
+  ],
   imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage, KatigoriaViewPage, ArtcleViewPage, Splash],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    FavoritesPage,
+    TabsPage,
+    KatigoriaViewPage,
+    ArtcleViewPage,
+    Splash
+  ],
   providers: [StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
 export class AppModule {}
--- src/pages/artcle-view/artcle-view.ts
+++ src/pages/artcle-view/artcle-view.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FavoritesPage } from '../favorites/favorites';
 
 /**
  * Generated class for the ArtcleViewPage page.
@@ -10,15 +11,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @IonicPage()
 @Component({
   selector: 'page-artcle-view',
-  templateUrl: 'artcle-view.html',
+  templateUrl: 'artcle-view.html'
 })
 export class ArtcleViewPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ArtcleViewPage');
   }
-
+  OpenFavs() {
+    this.navCtrl.push(FavoritesPage);
+  }
 }
